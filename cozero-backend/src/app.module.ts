470e7b85b3c4405aa8e3a65ce3933a84b647e65b
LucasDibz <lucas.f.dib@gmail.com>
2023-08-16 12:34:49 +0100

    fix: lint

--- cozero-backend/src/app.module.ts
+++ cozero-backend/src/app.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { Project } from './projects/entities/project.entity';
 import { ProjectsModule } from './projects/projects.module';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
-import { JwtAuthGuard } from './auth/jwt-auth.guard';
-import { APP_GUARD } from '@nestjs/core';
 import { User } from './users/entities/user.entity';
-import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
